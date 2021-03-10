722e91126a37b89aac056cbe5bc3f8da0de998d2
minoru.tomioka <275practice@gmail.com>
2021-03-10 14:06:06 +0000

    user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'topppages#index'
+  root to: 'toppages#index'
+  
+  get 'signup', to: 'users#new'
+  resources :users,only: [:index, :show, :create]
 
 
 end
