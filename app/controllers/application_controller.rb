f8ee56e91b097553c1cc94afcd08925a53364505
minoru.tomioka <275practice@gmail.com>
2021-03-19 13:56:44 +0000

    post

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,6 +12,8 @@ class ApplicationController < ActionController::Base
   
   def counts(user)
     @count_microposts = user.microposts.count
-  end    
+    @count_followings = user.followings.count
+    @count_followers = user.followers.count
+  end
     
 end
