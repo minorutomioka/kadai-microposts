e62dc62f73b14ded2e05c52df6ff0ddc5418327f
minoru.tomioka <275practice@gmail.com>
2021-03-30 11:41:53 +0000

    microposts 3

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,8 +1,6 @@
 class FavoritesController < ApplicationController
     before_action :require_user_logged_in
-    before_action :set_micropost
-    
-    
+
   def create
     @favorite = current_user.favorites.build(micropost_id: params[:micropost_id])
     @favorite.save
@@ -11,17 +9,10 @@ class FavoritesController < ApplicationController
   end
 
   def destroy
+    @micropost = Micropost.find(params[:micropost_id])
     @favorite = current_user.favorites.find_by(micropost_id: @micropost.id)
     @favorite.destroy
     flash[:success] = 'お気に入りから外しました。'
     redirect_back(fallback_location: root_path)
   end
-  
-  private
-  
-  def set_micropost
-    @micropost = Micropost.find(params[:micropost_id])
-  end
-  
-  
 end
